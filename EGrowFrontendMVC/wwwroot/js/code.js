11e9ca481a897c1e7eb45da8b79b30aed646b2ef
lord-kappa <jan.urbanc4@gmail.com>
2022-03-10 14:42:02 +0100

    Odjava in javascript

---
+++ EGrowFrontendMVC/wwwroot/js/code.js
@@ -0,0 +1,54 @@
+﻿window.addEventListener("DOMContentLoaded", (e) => {
+
+    function getCookie(name) {
+        var cookieArr = document.cookie.split(";");
+        for (var i = 0; i < cookieArr.length; i++) {
+            var cookiePair = cookieArr[i].split("=");
+            if (name == cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+        return null;
+    }
+    
+
+    function checkCookie() {
+        var username = getCookie("username");
+        console.log("zooooo " + username);
+
+        //username je v cookiju
+        if (username != null) {
+            var loginElement = document.getElementById("loginElement");
+            var registrationElement = document.getElementById("registracijaElement");
+            var odjavaElement = document.getElementById("odjavaElement");
+
+            loginElement.style.display = "none";
+            registrationElement.style.display = "none";
+            odjavaElement.style.display = "block";
+
+        }
+        else {
+            var loginElement = document.getElementById("loginElement");
+            var registrationElement = document.getElementById("registracijaElement");
+            var odjavaElement = document.getElementById("odjavaElement");
+
+            loginElement.style.display = "block";
+            registrationElement.style.display = "block";
+            odjavaElement.style.display = "none";
+        }
+    }
+
+    function clickOdjava() {
+        var odjavaElement = document.getElementById("odjavaElement");
+        odjavaElement.addEventListener("click", odjava)
+    }
+
+    function odjava() {
+        document.cookie = "username= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+        document.cookie = "userGuid= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+    }
+
+    checkCookie();
+    clickOdjava();
+
+});
