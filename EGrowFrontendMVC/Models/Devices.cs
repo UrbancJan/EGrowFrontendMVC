5fb6e148d9f5773e5cd26f2ee8a9d4d80650ef31
lord-kappa <jan.urbanc4@gmail.com>
2022-03-10 10:06:09 +0100

    Dodal models

---
+++ EGrowFrontendMVC/Models/Devices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EGrowFrontendMVC.Models
+{
+    public class Devices
+    {
+        public string deviceId { get; set; }
+    }
+}
