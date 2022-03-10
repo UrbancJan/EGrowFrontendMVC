2ac149c74cf948f699d9f1a7ad8545471ed08de2
lord-kappa <jan.urbanc4@gmail.com>
2022-03-10 10:30:09 +0100

    Login (nedokoncano)

---
+++ EGrowFrontendMVC/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using EGrowFrontendMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace EGrowFrontendMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44319/api/Login");
+                var login = client.PostAsJsonAsync<User>("user", user);
+                    login.Wait();
+                var result = login.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Napaka");
+            return View();
+        }
+    }
+}
